b6bf239b7dfd3bc91340ea4bba5103b6096c5176
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-11-18 15:22:52 +0700

    MySurvey 2

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/mysurvey-data', [App\Http\Controllers\MainController::class, 'data']);
+Route::get('/assess/{id}', [App\Http\Controllers\MainController::class, 'showasses']);
 
 Route::resources(['dash'=> MainController::class]);
