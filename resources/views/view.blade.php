19c215fc614b84e88ceffad570b44f249b97b12a
erwansyah01 <andienfirman526@gmail.com>
2021-11-24 14:34:40 +0700

    view profile

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -3,7 +3,6 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- <link rel="stylesheet" href="/assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css"> -->
-<link rel="stylesheet" href="/assets/vendor/datatables2/datatables.min.css" />
 <link rel="stylesheet" href="/assets/vendor/@fortawesome/fontawesome-free/css/fontawesome.min.css" />
 
 @endsection
